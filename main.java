532c0bd7d2bbc9aa131f814172893e5af966aed0
jthalacker <jthalacker@DoIT-7J0ZG3QC-W.ad.wisc.edu>
2019-04-22 17:03:12 -0500

    just for the hell of it

--- main.java
+++ main.java
@@ -4,9 +4,5 @@
 //Hunter Celeste has editted this
 // alex edit is the best
 //I have overwritten alex's line
-// Rose is trying to push 
-public class main {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
+
+//Hey this wont work
