18424fea53048d4be4e1bdc9a41becd605f7435c
alexscherer <ascherer3@wisc.edu>
2019-04-22 16:45:46 -0500

    conflict trial # 602

--- main.java
+++ main.java
@@ -4,3 +4,8 @@
 //Hunter Celeste has editted this
 // alex edit is the best
 //I have overwritten alex's line
+public class main {
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
