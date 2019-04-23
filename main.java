87673bedae976faf87a510da82675bd8202b0fd0
rtokar <rtokar@wisc.edu>
2019-04-23 11:01:48 -0500

    bigger screen

--- main.java
+++ main.java
@@ -66,7 +66,7 @@ public class main extends Application{
       root.getChildren().addAll(vBox1, vBox2);
       
       //Create scene and update stage
-      Scene scene = new Scene(root,350,300);
+      Scene scene = new Scene(root,800,600);
       primaryStage.setScene(scene);
       primaryStage.setTitle("Quiz Generator");
       primaryStage.show();
